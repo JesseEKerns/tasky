7b653d4af7cb0ee4627ca906a170cc4fe4591bcc
Jesse Kerns <jesseekerns@gmail.com>
2011-10-23 10:26:51 -0400

    Fixed coding format

--- Task.rb
+++ Task.rb
@@ -1,35 +1,35 @@
 class Task
-	
+
   def initialize(task_string)
-		@name = task_string
-	end
-	
-	def save
-		File.open("Tasks.txt", "a") do |file|
-			file.puts @name
-		end
-	end
-	
-	def self.list_tasks
+    @name = task_string
+  end
+
+  def save
+    File.open("Tasks.txt", "a") do |file|
+      file.puts @name
+    end
+  end
+
+  def self.list_tasks
     File.open("Tasks.txt", "r") do |file|
       file.each_line do |line| 
         puts "#{file.lineno}: #{line}"
       end
     end
   end
-	
+
   def self.complete_task(task_number)
-    #open the file 
+  #open the file 
     line_array = File.readlines("Tasks.txt")
-      line_array.delete_at(task_number - 1)
-		File.open("Tasks.txt", "w") do |file|
-			line_array.each{|line| file.puts(line)}
+    line_array.delete_at(task_number - 1)
+    File.open("Tasks.txt", "w") do |file|
+      line_array.each{|line| file.puts(line)}
     end    	
-    
+
     puts "You completed task number #{task_number}!"
-    
+
     print_congrats
-    
+
   end
   def self.print_congrats
     cmessages = ["Rock On!", "Oh Yeah!", "Woot woot!", "Ohhh"]
--- Tasky.rb
+++ Tasky.rb
@@ -1,28 +1,29 @@
 require_relative "Task.rb"
-		def list_tasks
-		puts
-		puts "TASKS TO COMPLETE:"
-		puts "=" * 18
-		puts Task.list_tasks
+def list_tasks
+  puts
+  puts "TASKS TO COMPLETE:"
+  puts "=" * 18
+  puts Task.list_tasks
 end
 
 command = ARGV.first
 command2 = ARGV[1] 
-	
+
 case command
 when "add" 
-	print("What do you need to do?: ")
-	task_string = STDIN.gets
-	task = Task.new(task_string)
-	task.save
-	list_tasks
-when "list"
-	list_tasks
-when "complete"
-	Task.complete_task(command2.to_i)
+  print("What do you need to do?: ")
+  task_string = STDIN.gets
+  task = Task.new(task_string)
+  task.save
   list_tasks
-  
+
+when "list"
+  list_tasks
+when "complete"
+  Task.complete_task(command2.to_i)
+  list_tasks
+
 else 
-	print("Re-type command: ")
-	abort
+  print("Re-type command: ")
+  abort
 end	
